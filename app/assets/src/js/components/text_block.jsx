e6da83481fd8fddf667dd8aeae62314e729c3517
bjryan2 <bjryan2@illinois.edu>
2014-09-29 11:28:53 -0500

    alignment for text blocks

--- app/assets/src/js/components/text_block.jsx
+++ app/assets/src/js/components/text_block.jsx
@@ -8,13 +8,16 @@ var textBlock = React.createClass({
 
   getDefaultProps: function() {
     return {
+      align: "left"
     };
   },
 
   render: function() {
+
+    var textClass = 'text-block ' + 'align-' + this.props.align;
     return (
       <Grid>
-        <Row className="text-block">
+        <Row className={textClass}>
           <Col md={8} mdOffset={2}>
             {this.props.children}
           </Col>
