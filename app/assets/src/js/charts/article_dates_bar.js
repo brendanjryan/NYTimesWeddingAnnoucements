f30d539396adc058772968844dac867ce46c5663
bjryan2 <bjryan2@illinois.edu>
2014-11-28 23:40:46 -0500

    dates bar

---
+++ app/assets/src/js/charts/article_dates_bar.js
@@ -0,0 +1,73 @@
+var d3 = require('d3');
+var _ = require('underscore');
+var $ = require('jquery');
+var colors = require('../helpers/colors');
+
+var chart = {};
+
+chart.run = function(mount, dataPath, width, height) {
+  d3.json(dataPath, function(data){
+    line_chart.run(mount, data, width, height);
+  });
+}
+
+var line_chart = (function(d3){
+
+  var svg = null;
+  var fill = d3.scale.linear();
+
+
+var run = function(mount, data, width, height) {
+
+   // convert all years to numbers
+
+  data.forEach(function(d){
+    d.year = +d.year
+  });
+
+    var chart = nv.models.multiBarChart()
+        .x(function(d) { return d.year })
+        .y(function(d) { return d.count })
+        .showLegend(false)
+        .showControls(false)
+        .tooltips(false)
+        .transitionDuration(500)
+        .reduceXTicks(true)
+        .staggerLabels(true)
+        .rotateLabels(45)
+        .height(height)
+        .width(width)
+        .groupSpacing(.1)
+      ;
+
+
+    chart.xAxis
+      .axisLabel("Year")
+      .tickPadding(10)
+    ;
+    chart.yAxis
+      .axisLabel("Number of Articles")
+      .tickFormat(d3.format(',.1f'))
+      .axisLabelDistance(10);
+    ;
+
+    var obj = {
+      key: 'poop',
+      color: colors.medGray,
+      values: data
+    };
+
+    d3.select(mount).append('svg')
+       .attr('width', width)
+      .attr('height', height)
+      .datum([obj])
+      .call(chart);
+
+  }
+
+  var res = {};
+  res.run = run;
+  return res;
+})(d3);
+
+module.exports = chart;
